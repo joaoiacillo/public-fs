ce06ac0e08fdff1e3679f939505e082d6e0b2f5f
João Iacillo <joaopiacillo@outlook.com.br>
2023-11-17 15:55:03 -0300

    Add files logic

--- server/routes/files.js
+++ server/routes/files.js
@@ -1,22 +1,67 @@
 const crypto = require("crypto");
 const upload = require("../upload");
+const db = require("../dev-db");
+const path = require("path");
 const { Router, response } = require("express");
 const router = Router();
 
+
 router.get("/", (req, res) => {
+    const files = db.queryAll();
     let responseData = {
-        files: [],
-        length: 0
+        files,
+        length: files.length
     };
 
     res.status(200).json(responseData);
 });
 
-router.post("/", upload.single("file"), (req, res) => {
-    const fileData = {
-        name: req.body.name,
-        content: req.file
+router.get("/:id", (req, res, next) => {
+    const id = req.params.id;
+    const file = db.queryById(id);
+    const exists = typeof file !== "undefined";
+    
+    let responseData = {
+        file
     }
+
+    if (!exists) responseData.message = "The file does not exist";
+
+    res.status(exists ? 200 : 404).json(responseData);
+});
+
+router.get("/:id/content", (req, res, next) => {
+    const id = req.params.id;
+    const file = db.queryById(id);
+    
+    if (typeof file === "undefined") {
+        let responseData = {
+            message: "The file does not exist",
+            id
+        }
+        res.status(404).json(responseData);
+        return;
+    }
+    
+    const options = {
+        root: path.join(__dirname, '..', 'files')
+    };
+
+    res.status(200).sendFile(file.filename, options, (err) => {
+        if (err) next(err);
+    });
+});
+
+router.post("/", upload.single("file"), (req, res) => {
+    const { filename, originalname, size } = req.file;
+
+    const fileData = {
+        id: filename,
+        name: originalname,
+        size
+    }
+
+    db.insert(filename, originalname, filename, size);
 
     let responseData = {
         message: "File created successfully.",
@@ -26,15 +71,17 @@ router.post("/", upload.single("file"), (req, res) => {
     res.status(201).json(responseData);
 });
 
-router.delete("/:uuid", (req, res) => {
-    const uuid = req.params.uuid;
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+
+    const result = db.remove(id)
 
     let responseData = {
-        message: "The file was deleted.",
-        uuid: uuid
+        message: result ? "The file was deleted." : "The file does not exist",
+        id: id
     };
 
-    res.status(200).json(responseData);
+    res.status(result ? 200 : 404).json(responseData);
 });
 
 module.exports = router;
